9f23ce4824e70579556e974200644fb7beec9ba4
Link4Electronics <link4electronics@gmail.com>
2026-02-26 18:33:04 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,12 +8,10 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/pixmaps/foobillardplus.png
-export DESKTOP=/usr/share/applications/foobillardplus.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/foobillardplus
+quick-sharun ./AppDir/binfoobillardplus
 
 # Additional changes can be done in between here
 
