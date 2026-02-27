5dd3fd56ebdf6e79f6ccce46bc9e2b508763c640
Link4Electronics <link4electronics@gmail.com>
2026-02-26 22:02:49 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export STARTUPWMCLASS=
+export STARTUPWMCLASS=foobillardplus
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
